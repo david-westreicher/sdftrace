bb3915fd718b15d6d98962f4538fb0f8670bf6d0
David Westreicher <david.westreicher@gmail.com>
2016-01-22 09:49:05 +0100

    move sdf to own file

--- js/renderer.js
+++ js/renderer.js
@@ -2,129 +2,28 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var stats;
 
-var cam, drawSDF, renderer;
-var drawCirclePass, drawCircleUs;
-var drawRectPass, drawRectUs;
-var resetPass;
-var drawSDFUs;
-
+var cam, renderer;
 var mouseX = 0, mouseY = 0;
-
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
-
 var size = 400;
-var pingpong = function(){
-    var self = this;
-    this.rts = [];
-    this.pointer = 0;
-    for(var i =0;i<2;i++){
-	    var rt = new THREE.WebGLRenderTarget(size,size, { 
-	        depthBuffer: false,
-	        stencilBuffer: false,
-	        minFilter: THREE.NearestFilter,
-	        magFilter: THREE.NearestFilter,
-	        format: THREE.RGBFormat,
-	        type:THREE.FloatType
-	    });
-	    rts.push(rt);
-    }
-    this.next = function(){
-        pointer = (pointer+1)%2;
-        return self.current(); 
-    }
-    this.current = function(){
-        return rts[pointer];
-    }
-    return self;
-}();
+var sdf;
 
 init();
 animate();
 
-function createFullScreenScene(shader){
-	var plane = new THREE.PlaneBufferGeometry(2,2);
-	var quad = new THREE.Mesh( plane, shader );
-	var tmpScene = new THREE.Scene();
-	tmpScene.add( quad );
-	return tmpScene;
-}
-
-function makeDrawCirclePass(){
-    drawCircleUs = {
-		pingpong: { type: "t", value: pingpong.current() },
-		circle: {type:"v3", value: new THREE.Vector3(size/2,size/2,size/2)}
-    }
-	var shader = new THREE.ShaderMaterial( {
-		uniforms: drawCircleUs,
-		vertexShader: document.getElementById( 'vertexFullscreen' ).textContent,
-		fragmentShader: document.getElementById( 'fragmentFillSDF' ).textContent,
-		depthWrite: false
-	} );
-	return createFullScreenScene(shader);
-}
-
-function makeDrawRectPass(){
-    drawRectUs = {
-		pingpong: { type: "t", value: pingpong.current() },
-		rect: {type:"v4", value: new THREE.Vector4(size/2,size/2,size/2,size/2)}
-    }
-	var shader = new THREE.ShaderMaterial( {
-		uniforms: drawRectUs,
-		vertexShader: document.getElementById( 'vertexFullscreen' ).textContent,
-		fragmentShader: document.getElementById( 'fragmentDrawRectSDF' ).textContent,
-		depthWrite: false
-	} );
-	return createFullScreenScene(shader);
-}
-
-function makeResetPass(){
-	var shader = new THREE.ShaderMaterial({
-		uniforms: {
-		    val: {type:"f", value: size*size}
-		},
-		vertexShader: document.getElementById( 'vertexFullscreen' ).textContent,
-		fragmentShader: document.getElementById( 'fragmentResetSDF' ).textContent,
-		depthWrite: false
-	});
-	return createFullScreenScene(shader);
-}
-
-function makeDrawPass(){
-    drawSDFUs = {
-		    tex: { type: "t", value: pingpong.current() },
-		    size: { type: "f", value: size },
-	}
-	var materialScreen = new THREE.ShaderMaterial( {
-		uniforms: drawSDFUs,
-		vertexShader: document.getElementById( 'vertexShader' ).textContent,
-		fragmentShader: document.getElementById( 'fragmentDrawSDF' ).textContent,
-		depthWrite: false
-	} );
-	var plane = new THREE.PlaneBufferGeometry( size,size );
-	var quad = new THREE.Mesh( plane, materialScreen );
-	var tmpScene = new THREE.Scene();
-	tmpScene.add( quad );
-	return tmpScene;
-}
-
 function init() {
-
-	var container = document.getElementById( 'container' );
+    sdf = new SDF(400);
 
 	cam = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, -10000, 10000 );
 	cam.position.z = 100;
-
-	drawSDF = makeDrawPass();
-	drawCirclePass = makeDrawCirclePass();
-	drawRectPass = makeDrawRectPass();
-	resetPass = makeResetPass();
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.autoClear = false;
 
+	var container = document.getElementById( 'container' );
 	container.appendChild( renderer.domElement );
 
 	stats = new Stats();
@@ -146,39 +45,13 @@ function animate() {
 	stats.update();
 }
 
-function generateSDF() {
-    // render dist size*size into rtPong
-	renderer.render( resetPass, cam, pingpong.current(), true );
-    drawCircleUs.circle.value.x = mouseX+size/2;
-    drawCircleUs.circle.value.y = -mouseY+size/2;
-    drawCircleUs.circle.value.z = size/8;
-    drawCircleUs.pingpong.value = pingpong.current();
-	renderer.render( drawCirclePass, cam, pingpong.next(), true );
-    drawCircleUs.circle.value.x = mouseX+size/2;
-    drawCircleUs.circle.value.y = mouseY+size/2;
-    drawCircleUs.circle.value.z = size/8;
-    drawCircleUs.pingpong.value = pingpong.current();
-	renderer.render( drawCirclePass, cam, pingpong.next(), true );
-    drawCircleUs.circle.value.x = -mouseX+size/2;
-    drawCircleUs.circle.value.y = -mouseY+size/2;
-    drawCircleUs.circle.value.z = size/8;
-    drawCircleUs.pingpong.value = pingpong.current();
-	renderer.render( drawCirclePass, cam, pingpong.next(), true );
-    drawRectUs.rect.value.x = size/2;
-    drawRectUs.rect.value.y = size/2;
-    drawRectUs.rect.value.z = 50;
-    drawRectUs.rect.value.w = 50;
-    drawRectUs.pingpong.value = pingpong.current();
-	renderer.render( drawRectPass, cam, pingpong.next(), true );
-}
-
 var isinit = true;
 function render() {
 	renderer.clear();
-	if(isinit){
-	    isinit = false;
-	}
-	generateSDF();
-	drawSDFUs.tex.value = pingpong.current();
-	renderer.render( drawSDF, cam );
+	sdf.clear(renderer);
+	sdf.drawCircle(renderer,mouseX+size/2,-mouseY+size/2,size/8);
+	sdf.drawCircle(renderer,mouseX+size/2,mouseY+size/2,size/8);
+	sdf.drawCircle(renderer,-mouseX+size/2,-mouseY+size/2,size/8);
+	sdf.drawRect(renderer,size/2,size/2,50,50);
+	sdf.draw(renderer,cam);
 }
---
+++ js/sdf.js
@@ -0,0 +1,123 @@
+var PingPong = function(){
+    var self = this;
+    self.rts = [];
+    self.pointer = 0;
+    for(var i =0;i<2;i++){
+	    var rt = new THREE.WebGLRenderTarget(size,size, { 
+	        depthBuffer: false,
+	        stencilBuffer: false,
+	        minFilter: THREE.NearestFilter,
+	        magFilter: THREE.NearestFilter,
+	        format: THREE.RGBFormat,
+	        type:THREE.FloatType
+	    });
+	    self.rts.push(rt);
+    }
+
+    self.next = function(){
+        self.pointer = (self.pointer+1)%2;
+        return self.current(); 
+    }
+
+    self.current = function(){
+        return self.rts[self.pointer];
+    }
+};
+
+var SDF = function(size){
+    var self = this;
+    self.pingpong = new PingPong();
+    self.size = size;
+	self.dummycam = new THREE.OrthographicCamera(0,0);
+	self.drawSDF = makeDrawPass();
+	self.drawCirclePass = makeDrawCirclePass();
+	self.drawRectPass = makeDrawRectPass();
+	self.resetPass = makeResetPass();
+
+    function createFullScreenScene(shader){
+	    var plane = new THREE.PlaneBufferGeometry(2,2);
+	    var quad = new THREE.Mesh( plane, shader );
+	    var tmpScene = new THREE.Scene();
+	    tmpScene.add( quad );
+	    return tmpScene;
+    }
+
+    function makeDrawCirclePass(){
+        self.drawCircleUs = {
+		    pingpong: { type: "t", value: self.pingpong.current() },
+		    circle: {type:"v3", value: new THREE.Vector3(size/2,size/2,size/2)}
+        }
+	    var shader = new THREE.ShaderMaterial( {
+		    uniforms: self.drawCircleUs,
+		    vertexShader: document.getElementById( 'vertexFullscreen' ).textContent,
+		    fragmentShader: document.getElementById( 'fragmentFillSDF' ).textContent,
+		    depthWrite: false
+	    } );
+	    return createFullScreenScene(shader);
+    }
+
+    function makeDrawRectPass(){
+        self.drawRectUs = {
+		    pingpong: { type: "t", value: self.pingpong.current() },
+		    rect: {type:"v4", value: new THREE.Vector4(size/2,size/2,size/2,size/2)}
+        }
+	    var shader = new THREE.ShaderMaterial( {
+		    uniforms: self.drawRectUs,
+		    vertexShader: document.getElementById( 'vertexFullscreen' ).textContent,
+		    fragmentShader: document.getElementById( 'fragmentDrawRectSDF' ).textContent,
+		    depthWrite: false
+	    } );
+	    return createFullScreenScene(shader);
+    }
+
+    function makeResetPass(){
+	    var shader = new THREE.ShaderMaterial({
+		    uniforms: {
+		        val: {type:"f", value: size*size}
+		    },
+		    vertexShader: document.getElementById( 'vertexFullscreen' ).textContent,
+		    fragmentShader: document.getElementById( 'fragmentResetSDF' ).textContent,
+		    depthWrite: false
+	    });
+	    return createFullScreenScene(shader);
+    }
+
+    function makeDrawPass(){
+        self.drawSDFUs = {
+		        tex: { type: "t", value: self.pingpong.current() },
+		        size: { type: "f", value: size },
+	    }
+	    var materialScreen = new THREE.ShaderMaterial( {
+		    uniforms: self.drawSDFUs,
+		    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+		    fragmentShader: document.getElementById( 'fragmentDrawSDF' ).textContent,
+		    depthWrite: false
+	    } );
+	    var plane = new THREE.PlaneBufferGeometry( size,size );
+	    var quad = new THREE.Mesh( plane, materialScreen );
+	    var tmpScene = new THREE.Scene();
+	    tmpScene.add( quad );
+	    return tmpScene;
+    }
+
+    self.clear = function(renderer){
+	    renderer.render( self.resetPass, self.dummycam, self.pingpong.current(), true );
+    }
+
+    self.drawCircle = function(renderer,x,y,size){
+        self.drawCircleUs.circle.value.set(x,y,size);
+        self.drawCircleUs.pingpong.value = self.pingpong.current();
+	    renderer.render( self.drawCirclePass, self.dummycam, self.pingpong.next(), true );
+    }
+
+    self.drawRect = function(renderer,x,y,width,height){
+        self.drawRectUs.rect.value.set(x,y,width,height);
+        self.drawRectUs.pingpong.value = self.pingpong.current();
+	    renderer.render( self.drawRectPass, self.dummycam, self.pingpong.next(), true );
+    }
+
+    self.draw = function(renderer,cam){
+        self.drawSDFUs.tex.value = self.pingpong.current();
+	    renderer.render( self.drawSDF, cam );
+    }
+}
